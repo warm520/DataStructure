4e62677f8ee682ffdf8e80e820adbbcceac8255f
chy_520 <itcast@chy.pw>
2022-11-14 16:09:26 +0800

    二叉树

---
+++ day01/src/com/chy/Code_FindFirstNode.java
@@ -0,0 +1,168 @@
+package com.chy;
+
+/**
+ * @description
+ * @autor chy
+ * @create 2022--11--14 15:05
+ */
+public class Code_FindFirstNode {
+    public static class Node{
+        public int value;
+        public Node next;
+        public Node(int data){
+            this.value = data;
+        }
+    }
+//    返回第一个交点
+    public static Node getIntersectNode(Node head1,Node head2){
+        if(head1==null||head2==null){
+            return null;
+        }
+//        得到入环节点
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+        if(loop1 == null && loop2==null){
+            return noLoop(head1,head2);
+        }
+        if(loop1!=null && loop2!=null){
+            return bothLoop(head1,loop1,head2,loop2);
+        }
+        return null;
+    }
+//    有环链表和无环链表不能相交
+//    如果两个链表都无环且相交则返回第一个交点 两个五环
+    public static Node noLoop(Node head1,Node head2){
+        if(head1==null || head2==null){
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while(cur1.next!=null){
+            n++;
+            cur1 = cur1.next;
+        }
+        while(cur2.next!=null){
+            n--;
+            cur2 = cur2.next;
+        }
+        if(cur1!=cur2){
+            return null;
+        }
+        cur1 = n>0? head1:head2; //谁长谁的头为cur1
+        cur2 = cur1==head1? head2:head1;//短的为cur2
+        n = Math.abs(n);
+        while(n!=0){
+            n--;
+            cur1 = cur1.next;
+        }
+        while(cur1!=cur2){
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+//    判断一个链是否有环并输出第一个入环节点
+    public static Node getLoopNode(Node head){
+        if(head==null || head.next==null || head.next.next==null){
+            return null;
+        }
+        Node slow = head.next;//慢指针
+        Node fast = head.next.next;//快指针
+        while(slow!=fast){
+            if(fast.next==null || fast.next.next==null){
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = head;
+        while(slow!=fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+//    两个有环链表，返回第一个交点没有则为null
+    public static Node bothLoop(Node head1,Node loop1,Node head2,Node loop2){
+        Node cur1 = null;
+        Node cur2 = null;
+        //入环节点为同一个
+        if(loop1==loop2){
+            cur1 = head1;
+            cur2 = head2;
+            int  n =0;
+            while(cur1!=loop1){
+                n++;
+                cur1 = cur1.next;
+            }
+            while(cur2!=loop2){
+                n--;
+                cur2 = cur2.next;
+            }
+            cur1 = n>0?head1:head2;
+            cur2 = cur1==head1?head2:head1;
+            n = Math.abs(n);
+            while(n!=0){
+                n--;
+                cur1 = cur1.next;
+            }
+            while(cur1!=cur2){
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        }else{
+            //入环节点不是同一个
+            cur1 = loop1.next;
+            while(cur1!=loop1){
+                if(cur1==loop2){
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        // 1->2->3->4->5->6->7->null
+        Node head1 = new Node(1);
+        head1.next = new Node(2);
+        head1.next.next = new Node(3);
+        head1.next.next.next = new Node(4);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(6);
+        head1.next.next.next.next.next.next = new Node(7);
+
+        // 0->9->8->6->7->null
+        Node head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next.next.next.next.next; // 8->6
+//        System.out.println(getIntersectNode(head1, head2).value);
+
+        // 1->2->3->4->5->6->7->4...
+        head1 = new Node(1);
+        head1.next = new Node(2);
+        head1.next.next = new Node(3);
+        head1.next.next.next = new Node(4);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(6);
+        head1.next.next.next.next.next.next = new Node(7);
+        head1.next.next.next.next.next.next = head1.next.next.next; // 7->4
+
+        // 0->9->8->2...
+        head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next; // 8->2
+        System.out.println(getIntersectNode(head1, head2).value);
+
+        // 0->9->8->6->4->5->6..
+        head2 = new Node(0);
+        head2.next = new Node(9);
+        head2.next.next = new Node(8);
+        head2.next.next.next = head1.next.next.next.next.next; // 8->6
+        System.out.println(getIntersectNode(head1, head2).value);
+    }
+}
---
+++ day01/src/com/chy/Code_PreInPost.java
@@ -0,0 +1,9 @@
+package com.chy;
+
+/**
+ * @description
+ * @autor chy
+ * @create 2022--11--14 16:08
+ */
+public class Code_PreInPost {
+}
