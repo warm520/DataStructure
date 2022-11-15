7edad7f13d90e1d9353b51068edf0047dd2615d8
chy_520 <itcast@chy.pw>
2022-11-15 10:11:32 +0800

    二叉树

---
+++ day01/src/com/chy/Code_TreeMaxWidth.java
@@ -0,0 +1,22 @@
+package com.chy;
+
+/**
+ * @description
+ * @autor chy
+ * @create 2022--11--14 17:11
+ */
+public class Code_TreeMaxWidth {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+    
+    public static void main(String[] args) {
+
+    }
+}
